8cb1eb928de2c988e13d1b8fbfb0fc5f017bb58a
Greg Woodfill <greg@gregwoodfill.com>
2018-03-14 19:06:38 -0500

    Adding react

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 // index.js
-hello = (world) => {
-    return 'hello ' + world
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const styles = {
+    app: {
+        paddingTop: 40,
+        textAlign: 'center',
+    },
 }
-document.write(hello("universe"))
+
+class App extends Component {
+    render() {
+        return (
+            <div style={styles.app}>
+                Welcome to React!
+            </div>
+        )
+    }
+}
+
+const root = document.querySelector('#app')
+ReactDOM.render(<App />, root)
